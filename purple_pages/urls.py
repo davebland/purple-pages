af330ab7095390cfeb3fd3deca3a2841aebfde9d
Dave Bland <david.bland@gmx.co.uk>
2019-10-08 18:26:52 +0100

    created first view with temporary route from website root

--- purple_pages/urls.py
+++ purple_pages/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.shortcuts import render
+
+# Temporary basic view to render a template
+def home(request):
+    return render(request, 'base.html')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home)
 ]
