f02953c518ab97e2a8359688f74369e57fc213e8
Dave Bland <david.bland@gmx.co.uk>
2019-10-18 21:13:12 +0100

    started html tiles on home home with dummy data passed in view

--- home/views.py
+++ home/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 
 def home(request):
     """ Generate Application Home Page """
-    return render(request, 'home.html', {'page_title':"Welcome To Purple Pages"})
+    return render(request, 'home.html', {'page_title':"Welcome To Purple Pages",
+        'ads':{'ad1':'detail1','ad2':'detail2', 'ad3':'detail3','ad4':'detail4'}
+        })
 
 def notice_boards(request):
     """ Generate Notice Boards Page """
--- static/js/home.js
+++ static/js/home.js
@@ -1,3 +1,4 @@
+// Show and hide menu on mobile
 $('.navbar-burger').click(function() {
     $('.navbar-burger').toggleClass('is-active');
     $('.navbar-menu').toggleClass('is-active');
