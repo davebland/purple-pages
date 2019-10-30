0b4c51ae39ee9fe3324df0bbb304ded300e5fc00
Dave Bland <david.bland@gmx.co.uk>
2019-10-30 12:03:37 +0000

    updated main url patterns to use boards rather than board/ for cross browser compatibility

--- purple_pages/urls.py
+++ purple_pages/urls.py
@@ -23,7 +23,7 @@ from tests import urls as urls_test
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name="home"),
-    path('boards', notice_boards, name="boards"),
-    path('boards/', include(urls_boards)),
+    path('boards/', notice_boards, name="boards"),
+    path('board/', include(urls_boards)),
     path('test/', include(urls_test))
 ]
