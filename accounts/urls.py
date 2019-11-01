f2043e230adb06cac8e02535bc109a1fefda6400
Dave Bland <david.bland@gmx.co.uk>
2019-11-01 11:22:57 +0000

    updated account views with page title in context

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,14 @@
 """ URL config for account views """
 
 from django.urls import path, include
+from django.contrib.auth import urls as auth_urls
+from django.contrib.auth import views as auth_views
 
 from .views import my_account, my_ads
 
 urlpatterns = [
     path('', my_account, name="my_account"),
     path('my_ads/', my_ads, name="my_ads"),
-    path('', include('django.contrib.auth.urls')),
+    path('login/', auth_views.LoginView.as_view(extra_context={'page_title':'Please Login'})),
+    path('', include(auth_urls)),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -3,8 +3,8 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def my_account(request):
-    return render(request, 'my_account.html')
+    return render(request, 'my_account.html', {'page_title':'Account Overview'})
 
 @login_required
 def my_ads(request):
-    return render(request, 'my_ads.html')
+    return render(request, 'my_ads.html', {'page_title':'My Adverts'})
