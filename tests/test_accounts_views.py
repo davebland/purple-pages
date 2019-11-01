a5cb65010cfd02ce30a1e4bf485c7880103748c0
Dave Bland <david.bland@gmx.co.uk>
2019-11-01 12:56:57 +0000

    logged in a user programatically for authenticated users test

--- tests/test_accounts_views.py
+++ tests/test_accounts_views.py
@@ -4,9 +4,6 @@ from django.contrib.auth.models import User
 class TestAccountViews(TestCase):
     """ Tests for account views """
 
-    # Setup a test user
-    #User.objects.create_user('pptestuser', 'test@test.com', 'localtest')
-
     def test_login_view(self):
         """ Test that login view for un-authenticated user returns Http response with correct template """
         page = self.client.get("/account/login/")
@@ -32,9 +29,13 @@ class TestAccountViews(TestCase):
 
     def test_authenticated_account_views(self):
         """ Test that all account views return correct page for authenticated users """
-        #self.client.login(username='pptestuser', password='localtest')
+        # Setup and login a test user
+        User.objects.create_user('pptestuser', 'test@test.com', 'localtest')
+        self.client.login(username='pptestuser', password='localtest')
+        # Get pages and confirm correct templates
         my_account = self.client.get("/account/", follow=True)
         my_ads = self.client.get("/account/my_ads", follow=True)
         change_password = self.client.get("/account/password_change", follow=True)      
-        #self.assertTemplateUsed(my_account, 'my_account.html')
-        #self.assertTemplateUsed(my_ads, 'my_ads.html')
+        self.assertTemplateUsed(my_account, 'my_account.html')
+        self.assertTemplateUsed(my_ads, 'my_ads.html')
+        self.assertTemplateUsed(change_password, 'registration/password_change_form.html')
