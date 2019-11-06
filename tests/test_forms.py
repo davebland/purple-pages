9c250bad434fa6202c139d4c9b40f09e55acc35b
Dave Bland <david.bland@gmx.co.uk>
2019-11-06 20:04:23 +0000

    test for create board form

--- tests/test_forms.py
+++ tests/test_forms.py
@@ -2,6 +2,8 @@ from django.test import TestCase
 
 from adverts.models import Advert
 from adverts.forms import AdvertForm
+from boards.models import Board
+from boards.forms import BoardForm
 
 class TestForms(TestCase):
     """ Tests for forms """
@@ -13,3 +15,8 @@ class TestForms(TestCase):
         """ Create an advert form, bind some data and check if valid """
         advert_form = AdvertForm(instance=Advert.objects.get(pk=1))
         self.assertTrue(advert_form.is_valid)
+
+    def test_boardForm(self):
+        """ Create a board form, bind some data and check if valid """
+        board_form = BoardForm(instance=Board.objects.get(pk=1))
+        self.assertTrue(board_form.is_valid)
