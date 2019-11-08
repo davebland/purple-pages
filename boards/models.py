4044fbc4c538cf6862c4d5a9411e14e7316ce2c1
Dave Bland <david.bland@gmx.co.uk>
2019-11-08 13:13:21 +0000

    moved postcodedistricts out to seperate model

--- boards/models.py
+++ boards/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 
+class PostCodeDistrict(models.Model):
+    """ Data model for postcodedistrict database """
+    postcode = models.CharField(max_length=4)
+
 class Board(models.Model):
     """ Data model for notice boards """
-    postCodeDistrict = models.CharField(max_length=4)
+    postCodeDistrict = models.OneToOneField(PostCodeDistrict, on_delete=models.PROTECT)
     name = models.CharField(max_length=30)    
     dateActive = models.DateField(auto_now_add=True)
 
     def __str__(self):
-        return self.postCodeDistrict
+        return self.postCodeDistrict.postcode
