5bf9247dae157af56696d3622855a62ad350351e
Dave Bland <david.bland@gmx.co.uk>
2019-11-08 19:43:39 +0000

    filter on BoardForm to show only postcodedistrict choices that do not already have a board

--- boards/forms.py
+++ boards/forms.py
@@ -1,8 +1,13 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, ModelChoiceField
 
-from .models import Board
+from .models import Board, PostCodeDistrict
 
 class BoardForm(ModelForm):
+
+    # Limit postCodeDistrict choices to those that don't already have a board
+    queryset = PostCodeDistrict.objects.filter(board=None)
+    postCodeDistrict = ModelChoiceField(queryset=queryset, empty_label="Select a postcode...")
+
     class Meta:
         model = Board
         fields = ['postCodeDistrict', 'name']
--- boards/urls.py
+++ boards/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('<int:board_pk>/', display_single_board, name="display_single_board"),
     path('<int:board_pk>/set_as_favourite/', set_favourite_board, {'set_unset':True}, name="set_favourite_board"),
     path('<int:board_pk>/unset_as_favourite/', set_favourite_board, name="unset_favourite_board"),
-    path('create_board', create_notice_board, name="create_notice_board"),
+    path('create_board/', create_notice_board, name="create_notice_board"),
 ]
