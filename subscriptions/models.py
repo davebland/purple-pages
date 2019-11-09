17059f10f8e1a7d1aeae037c90f152c95d572ffb
Dave Bland <david.bland@gmx.co.uk>
2019-11-09 09:45:46 +0000

    moved subscription foreign key into payment model and updated template to use backward lookup

--- subscriptions/models.py
+++ subscriptions/models.py
@@ -1,14 +1,20 @@
 from django.db import models
 from django.conf import settings
 
+class Subscription(models.Model):
+    """ Data model for a user's subscription """
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+    expiry_date = models.DateField()
+
+    def __str__(self):
+        return "Subscription for user {}".format(self.pk)
+
 class Payment(models.Model):
     """ Data model for a record of payment """
+    user_subscription = models.ForeignKey(Subscription, on_delete=models.SET_NULL, null=True)
     payment_date = models.DateTimeField(auto_now_add=True)
     stripe_ref = models.CharField(max_length=30, null=True)
     amount = models.IntegerField(null=True)
 
-class Subscription(models.Model):
-    """ Data model for a user's subscription """
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
-    last_payment = models.OneToOneField(Payment, on_delete=models.SET_NULL, null=True)
-    expiry_date = models.DateField()
+    def __str__(self):
+        return "Payment for subscription {}".format(self.user_subscription.pk)
--- subscriptions/views.py
+++ subscriptions/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+from .models import Subscription, Payment
+
 @login_required
 def subscription_overview(request):
     """ Generate page showing an overview of the user's subscription """
-    return render(request, 'subscription_overview.html', {'page_title':'My Subscription'})
+    try:
+        user_subscription = Subscription.objects.get(user=request.user)
+    except:
+        user_subscription = "No subscription found"
+    return render(request, 'subscription_overview.html', {'page_title':'My Subscription', 'user_subscription':user_subscription})
