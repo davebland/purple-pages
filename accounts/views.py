7b026054a2935b740febe07be6f9e28f13d74c27
Dave Bland <david.bland@gmx.co.uk>
2019-11-09 19:49:34 +0000

    moved user stats to account overview

--- accounts/views.py
+++ accounts/views.py
@@ -3,10 +3,27 @@ from django.contrib.auth.decorators import login_required
 from django.template.loader import render_to_string
 
 from adverts.views import get_user_ads
+from adverts.models import Advert
+from subscriptions.models import Subscription
 
 @login_required
 def my_account(request):
-    return render(request, 'my_account.html', {'page_title':'Account Overview'})
+    """ Generate user account overview page """
+    # Collect some object to use in stats
+    try:
+        user_subscription = Subscription.objects.get(user=request.user)
+    except:
+        user_subscription = "No Subscription"
+    try:
+        user_ads = Advert.objects.filter(user=request.user).count()
+    except:
+        user_ads = "No Ads"
+    # Create stats dict and render
+    user_stats = {
+        'subscription' : user_subscription,
+        'no_of_ads' : user_ads,
+    }
+    return render(request, 'my_account.html', {'page_title':'Account Overview', 'user_stats':user_stats})
 
 @login_required
 def my_ads(request):
