d88fde706ad822773cc1445a750796d3f3322bf0
Dave Bland <david.bland@gmx.co.uk>
2019-11-10 09:48:47 +0000

    updated PPUser model to extend User more cleanly, cleared migrations for a fresh DB

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 from .models import PPUser
 
-admin.site.register(PPUser)
+# Inline admin descriptor
+class PPUserInline(admin.StackedInline):
+    model = PPUser
+    can_delete = False
+    verbose_name_plural = 'PPUser'
+
+# New User admin
+class UserAdmin(BaseUserAdmin):
+    inlines = (PPUserInline,)
+
+# Re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 from boards.models import Board
 
-class PPUser(User):
+class PPUser(models.Model):
     """ Data model for a Purple Pages User (extends django User) """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     favourite_board = models.ForeignKey(Board, on_delete=models.SET_NULL, null=True, blank=True)
