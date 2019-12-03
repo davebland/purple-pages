312314210f9c444f318f812e0d4ebdb9364655f7
Dave Bland <david.bland@gmx.co.uk>
2019-12-03 18:28:11 +0000

    added AdvertTemplate as foriegn key in Advert and updated to_html method to render template defined in object attribute

--- adverts/models.py
+++ adverts/models.py
@@ -7,11 +7,21 @@ from django.conf import settings
 
 from boards.models import Board
 
+class AdvertTemplate(models.Model):
+    """ Data model for an advert template """
+    name = models.CharField(max_length=30)
+    template_file = models.FilePathField(path=os.path.join(settings.BASE_DIR, "adverts/templates"),match="ad_template_*")
+    is_active = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
 class Advert(models.Model):
     """ Data model for adverts """
     title = models.CharField(max_length=30)
     textContent = models.TextField(default="")
     impression_counter = models.IntegerField(default=0)
+    template = models.ForeignKey(AdvertTemplate, on_delete=models.PROTECT, default=1)
     
     user = models.ForeignKey(User, on_delete=models.PROTECT)
     on_boards = models.ManyToManyField(Board)
@@ -21,7 +31,7 @@ class Advert(models.Model):
 
     def to_html(self):
         """ Rended the advert to a html string using the relevant template """
-        return render_to_string('ad_template_1.html', {'advert':self})
+        return render_to_string(self.template.template_file, {'advert':self})
 
     def board_list(self):
         """ Return a query set of boards this Advert appears on """
@@ -35,12 +45,3 @@ class Advert(models.Model):
             self.save(update_fields=['impression_counter'])
         except:
             self.impression_counter = 0
-
-class AdvertTemplate(models.Model):
-    """ Data model for an advert template """
-    name = models.CharField(max_length=30)
-    template_file = models.FilePathField(path=os.path.join(settings.BASE_DIR, "adverts/templates"),match="ad_template_*")
-    is_active = models.BooleanField(default=False)
-
-    def __str__(self):
-        return self.name
