98d176be8e76b5a6e3caf4c0b40c59d660e64d80
Dave Bland <david.bland@gmx.co.uk>
2020-02-15 13:10:56 +0000

    started on refactoring advert add/edit with bulma styling

--- adverts/forms.py
+++ adverts/forms.py
@@ -1,9 +1,14 @@
-from django.forms import ModelForm, HiddenInput
+from django.forms import ModelForm, TextInput, Textarea, FileInput
 
 from .models import Advert
 
 class AdvertForm(ModelForm):
     class Meta:
         model = Advert
-        exclude = ['impression_counter']
-        widgets = {'user':HiddenInput()}
+        exclude = ['view_counter']
+        widgets = {
+            'title' : TextInput(attrs={'class':'input'}),
+            'strapline' : TextInput(attrs={'class':'input'}),
+            'text_content' : Textarea(attrs={'class':'textarea'}),
+            'image' : FileInput(attrs={'class':'file-input', 'element':'upload'}),
+        }
--- adverts/views.py
+++ adverts/views.py
@@ -6,23 +6,6 @@ from django.contrib.auth.decorators import login_required
 from .models import Advert
 from .forms import AdvertForm
 
-def get_ads(board_pk):
-    """ Return a tuple containing all adverts for specified board and update impression counter """
-    all_ads = Advert.objects.filter(on_boards=board_pk)
-    for ad in all_ads:
-        ad.add_impression()
-    return tuple(all_ads)
-
-def get_user_ads(uid):
-    """ Return a tuple containing all adverts with edit frames for specified user """
-    # Get the adverts
-    user_ads = Advert.objects.filter(user=uid)
-    # Rended each to html string with edit frame
-    user_ads_html = []
-    for ad in user_ads:
-        user_ads_html.append(render_to_string('advert_edit_frame.html', {'ad':ad}))
-    return tuple(user_ads_html)
-
 @login_required
 def advert_add_edit(request, advert_id=None):
     """ Generate add/edit page """
@@ -44,9 +27,9 @@ def advert_add_edit(request, advert_id=None):
     def edit_advert(request, advert_id):
         """ Get and advert and pass it to add_edit page as form """
         # Get the object only if the authenticated user is that same as the adverts user
-        advert = get_object_or_404(Advert, pk=advert_id, user=request.user)
+        advert = get_object_or_404(Advert, pk=advert_id, ppuser=request.user)
         advert_form = AdvertForm(instance=advert)
-        return render(request, 'advert_add_edit.html', {'page_title':'Edit Advert', 'advert_form':advert_form})
+        return render(request, 'advert_add_edit.html', {'advert_form':advert_form})
 
     if request.method == "POST":
         return save_advert(request, advert_id)
@@ -54,8 +37,8 @@ def advert_add_edit(request, advert_id=None):
         return edit_advert(request, advert_id)
     else:
         # If a new advert create a blank form and pass this into template
-        advert_form = AdvertForm(initial={'user': request.user})
-        return render(request, 'advert_add_edit.html', {'page_title':'New Advert', 'advert_form':advert_form})
+        advert_form = AdvertForm()
+        return render(request, 'advert_add_edit.html', {'advert_form':advert_form})
 
 @login_required
 def preview_advert(request):
