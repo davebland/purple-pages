453e6f9d9e64046faa76432db63edff6fb7d6cd2
Dave Bland <david.bland@gmx.co.uk>
2020-03-01 22:25:59 +0000

    work on stripe payment form handling

--- static/js/stripe.js
+++ static/js/stripe.js
@@ -1,53 +1,52 @@
 // Stripe elements for subscription payments
 
-function requestPayment(subscriptionFormData) {
+// Create Stripe payment elements in payment form when subscription form submitted
+$('#subscription-form').submit(function(subscriptionForm) {
+    // Collect form data
+    let subscriptionFormData = $(this).serializeArray();   
+    // Disable subscription form and display spinner
+    subscriptionForm.preventDefault();
+    $('#subscription-form :input').prop('disabled', true);
+    $('#subscribe-button').addClass('is-loading');
     // Request payment intent and setup a payment form
     $.post(subscriptionFormData[0].value, subscriptionFormData)
     .done(function(response) {
         // Create and display stripe payment elements
         console.log(response)
-        console.log(response)
+        let stripe = Stripe('pk_test_Tgy18iERVA1liKcyrLebR4sN00fdlqLNY0');
+        let stripeElements = stripe.elements();
+        let stripeStyle = {
+        };
+        let stripePaymentElements = stripeElements.create("card", {style:stripeStyle});
+        stripePaymentElements.mount("#stripe-card-element");
+        $('#stripe-payment-form').show(ANIMATION_DURATION);
+
+        // Listen for stripe input validation errors and display  
+        stripePaymentElements.addEventListener('change', ({error}) => {
+            if (error) {
+                $('#stripe-card-errors').text(error.message);
+                $('#stripe-card-errors').show(ANIMATION_DURATION);
+            } else {
+                $('#stripe-card-errors').hide(ANIMATION_DURATION);
+                $('#stripe-card-errors').empty();
+            }        
+        });
+
+        // Make a stripe payment when form submitted
+        $('#stripe-payment-form').submit(function(stripePaymentForm) {
+            // Disable subscription form and display spinner   
+            stripePaymentForm.preventDefault();
+            $('#stripe-payment-form :input').prop('disabled', true);
+            stripePaymentElements.update('disabled', true);
+            $('#stripe-submit').addClass('is-loading');
+        })
     })
     .fail(function(error) {
-        // Unable to request payment error
-        console.log(error.status)
+        // Show unable to request payment error        
+        $('#stripe-connection-error').show(ANIMATION_DURATION)
     })
-}
-
-
-let stripe = Stripe('pk_test_Tgy18iERVA1liKcyrLebR4sN00fdlqLNY0');
-let stripeElements = stripe.elements();
-
-// Create Stripe payment elements in payment form
-$('#subscription-form').submit(function(subscriptionForm) {
-    // Collect form data
-    let subscriptionFormData = $(this).serializeArray()
-    // Disable subscription form
-    subscriptionForm.preventDefault();
-    $('#subscription-form :input').prop('disabled', true)
-    // Request payment intent with form data   
-    requestPayment(subscriptionFormData)
-    
-    // Generate and display the payment form
-    let stripeStyle = {
-      };
-    let stripePaymentElements = stripeElements.create("card", {style:stripeStyle});
-    stripePaymentElements.mount("#stripe-card-element");
-
-    // Listen for stripe input validation errors and display  
-    stripePaymentElements.addEventListener('change', ({error}) => {
-        if (error) {
-            $('#stripe-card-errors').text(error.message);
-        } else {
-            $('#stripe-card-errors').empty();
-        }        
-    });      
-})
-
-// Handle payment cancel button
-$('#stripe-cancel').click(function(stripePaymentForm) {
-    stripePaymentForm.preventDefault();
-    // Clear payment form and reset subscription form
-    //document.getElementById("subscription-form").reset();  
-})
-
+    .always(function() {
+        // Clear loading spinner
+        $('#subscribe-button').removeClass('is-loading');
+    });
+});
