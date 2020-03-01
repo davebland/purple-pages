73e35c9ab50e84f615aa945a6fe4b6a0aeb48c69
Dave Bland <david.bland@gmx.co.uk>
2020-03-01 11:11:23 +0000

    work on stripe.js form handling

--- static/js/stripe.js
+++ static/js/stripe.js
@@ -1,13 +1,25 @@
 // Stripe elements for subscription payments
 
+function requestPayment(subscriptionFormData) {
+    // Request payment intent and setup a payment form
+    console.log(subscriptionFormData.payment-request-url.value)
+    $.post(subscriptionFormData[1]['value'], subscriptionFormData[0].serialize())
+}
+
+
 let stripe = Stripe('pk_test_Tgy18iERVA1liKcyrLebR4sN00fdlqLNY0');
 let stripeElements = stripe.elements();
 
 // Create Stripe payment elements in payment form
-$('#subscribe-button').click(function(subscriptionForm) {
+$('#subscription-form').submit(function(subscriptionForm) {
+    // Collect form data
+    let subscriptionFormData = $(this).serializeArray()
     // Disable subscription form
     subscriptionForm.preventDefault();
     $('#subscription-form :input').prop('disabled', true)
+    // Request payment intent with form data   
+    requestPayment(subscriptionForm)
+    
     // Generate and display the payment form
     let stripeStyle = {
       };
