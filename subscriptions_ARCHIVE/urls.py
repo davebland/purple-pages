9de7568b3ec084b8253b0009cf89aa1f0bb2d3af
Dave Bland <david.bland@gmx.co.uk>
2020-03-11 17:26:09 +0000

    del subscriptions and work on home page styling

--- subscriptions_ARCHIVE/urls.py
+++
@@ -1,8 +0,0 @@
-""" URL config for subscription views """
-
-from django.urls import path
-from .views import subscription_overview
-
-urlpatterns = [
-    path('', subscription_overview, name="subscription_overview"),
-]
