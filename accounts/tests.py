af1a2b623f56696b9e111f793ff861356abc6048
Dave Bland <david.bland@gmx.co.uk>
2020-03-31 22:09:54 +0100

    added coverage.py for analysis of tests

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,16 +1,18 @@
 from django.test import TestCase
+from django.http import JsonResponse, HttpResponseBadRequest
 
 from datetime import date, timedelta
 import json
 
 from .models import PPUser, Payment
 from .forms import PPUserCreationForm
+from .stripe import stripe_payment_intent, stripe_confirm_payment
 
 class TestAccountsModels(TestCase):
     """ Tests for accounts models """
 
-    def test_ppuser_subscription_status(self):
-        """ Create a user and test subscription status """
+    def test_ppuser_and_subscription_status(self):
+        """ Create user and test subscription status """        
         user = PPUser()
         user.save()
         self.assertEqual(user.subscription_status(), 0)
@@ -45,7 +47,7 @@ class TestAccountViews(TestCase):
         self.assertTemplateUsed(page, 'my_account.html')
         self.assertIsInstance(page.context['user_stats'], dict)
 
-    def test_account_view(self):
+    def test_my_ads_view(self):
         """ Test My Ads view returns correct template """
         # Create user and login
         PPUser.objects.create_user("test","test@test.com","test")
@@ -59,7 +61,7 @@ class TestAccountViews(TestCase):
         """ Test Registration view returns correct template """
         page = self.client.get("/account/registration/", follow=True)
         self.assertEqual(page.status_code, 200)
-        self.assertTemplateUsed(page, 'registration.html')
+        self.assertTemplateUsed(page, 'registration.html')        
 
     def test_subscription_view(self):
         """ Test subscription view returns correct template """
@@ -73,13 +75,16 @@ class TestAccountViews(TestCase):
 
     def test_create_subscription_view(self):
         """ Test Create Subscription view returns a valid JSON response """
-        page = self.client.get("/account/create_payment/", follow=True)
-        self.assertEqual(page.status_code, 200)
-        #self.assertEqual(, {})
+        # Create user and login
+        PPUser.objects.create_user("test","test@test.com","test")
+        self.client.login(username="test", password="test")
+        # test create subscription view
+        page = self.client.post("/account/create_payment/", {'subscription-period':100}, follow=True)
+        self.assertEqual(page.status_code, 200)    
 
     def test_stripe_webhook_view(self):
         """ Test Stripe webhook handler returns 400 when not presented with stripe payment intent """
-        endpoint = self.client.post("/account/confirm_payment/", follow=True)
+        endpoint = self.client.post("/account/confirm_payment/", {}, follow=True)
         self.assertEqual(endpoint.status_code, 400)
 
 class TestAccountForms(TestCase):
@@ -90,3 +95,16 @@ class TestAccountForms(TestCase):
         form = PPUserCreationForm({'username':'testuser', 'password1':'test password', 'password2':'test password'})
         self.assertTrue(form.is_valid())
         self.assertEqual(form.save(),PPUser.objects.get(pk=1))
+
+class TestStripe(TestCase):
+    """ Test for PP Stripe """
+
+    def test_stripe_payment_intent(self):
+        """ Test create payment intent """        
+        intent = stripe_payment_intent({'payment_amount':100,'period':100,'pp_username':1})
+        self.assertIsInstance(intent, JsonResponse)
+
+    def test_stripe_webhook(self):
+        """ Test handling webhook data (stripe_confirm_payment) """
+        webhook = stripe_confirm_payment({})
+        self.assertIsInstance(webhook, HttpResponseBadRequest)
